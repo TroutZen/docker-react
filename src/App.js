d4cea367fe5384908fa8d1f8f66f922414da715b
troutzen <devin.otway@amplitude.com>
2021-08-22 21:30:25 -0700

    forcing change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          testing 1234
+          testing 12345
         </a>
       </header>
     </div>
